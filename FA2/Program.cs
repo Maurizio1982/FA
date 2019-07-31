7791d9bf9257cc2c29a30c64e7d949aae299b067
Markku Lehtonen <markkuolavi82@gmail.com>
2019-07-31 13:43:00 +0300

    move repo dir

---
+++ FA2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
